37f0403414dba2e454130700617eeebee530ef0e
MichaelJamesParsons <mjay.parsons@gmail.com>
2017-11-05 16:16:15 -0500

    Implemented simple redux workflow

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
+import configureStore from './store/configureStore'
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import routes from './routes'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './styles/style.css'
 
-ReactDOM.render(<Router>{routes}</Router>, document.getElementById('root'))
+const store = configureStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
